20a16178e32da5a83d2ed49f433ff628e4b1d213
vitorhugoserra <vitorhugoserravalle@gmail.com>
2019-03-07 14:06:06 -0300

    get_data for binance.

---
+++ app/backend/get_data/dev_data/main.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# Module ?: 
+
+"""
+Deve ler o arquivo de dados de mercado, caso exista, ou criá-lo a partir de dados da corretora, respeitando os 
+parâmetros do arquivo de parâmetros.
+"""
+
+import sys
+import os
+
+sys.path.append(os.getcwd().split(sep="modules")[0])
+#import modules
+#from modules import func
---
+++ app/backend/get_data/dev_data/modules/db_data/__init__.py
@@ -0,0 +1,3 @@
+# coding: utf-8
+
+#__all__ = ['date_to_milliseconds','interval_to_milliseconds','get_historical_klines','get_historical_klines_from_ts']
